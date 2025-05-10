a42cd007e5a621b953dee9c066827a3c238e0b63
AsharKhursheed <asharkhursheed26@gmail.com>
2025-05-10 18:10:29 +0500

    changes

--- src/Components/InputFields/CheckBoxField.js
+++ src/Components/InputFields/CheckBoxField.js
@@ -1,13 +1,39 @@
-import { Field } from "formik";
+// import { Field } from "formik";
+// import React from "react";
+// import { FormGroup } from "reactstrap";
+// import InputWrapper from "../../Utils/HOC/InputWrapper";
+
+// const CheckBoxField = ({ name, helpertext }) => {
+//   return (
+//     <FormGroup switch className="ps-0 form-switch custom-switch-flex form-check">
+//       <label className="switch">
+//         <Field type="checkbox" name={name}/>
+//         <span className="switch-state"></span>
+//       </label>
+//       {helpertext && <p className="help-text">{helpertext}</p>}
+//     </FormGroup>
+//   );
+// };
+
+// export default InputWrapper(CheckBoxField);
+
 import React from "react";
+import { useField } from "formik";
 import { FormGroup } from "reactstrap";
 import InputWrapper from "../../Utils/HOC/InputWrapper";
 
 const CheckBoxField = ({ name, helpertext }) => {
+  const [field, , helpers] = useField({ name, type: 'checkbox' });
+
   return (
     <FormGroup switch className="ps-0 form-switch custom-switch-flex form-check">
       <label className="switch">
-        <Field type="checkbox" name={name}/>
+        <input
+          type="checkbox"
+          {...field}
+          checked={field.value || false}
+          onChange={(e) => helpers.setValue(e.target.checked)}
+        />
         <span className="switch-state"></span>
       </label>
       {helpertext && <p className="help-text">{helpertext}</p>}
