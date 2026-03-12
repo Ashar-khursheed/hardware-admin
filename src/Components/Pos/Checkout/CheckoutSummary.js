6c2b3c81cb2de45d40df9735d982807cf85fa35f
Ashar-khursheed <asharkhursheed26@gmail.com>
2026-03-13 03:11:33 +0500

    cgsd

--- src/Components/Pos/Checkout/CheckoutSummary.js
+++ src/Components/Pos/Checkout/CheckoutSummary.js
@@ -31,16 +31,16 @@ const CheckoutSidebar = ({ addToCartData, values, setFieldValue, data, loading, 
 
   // Submitting data on Checkout
   useEffect(() => {
-    if (values["billing_address_id"] && values["shipping_address_id"] && values["delivery_description"] && values["payment_method"]) {
+    if (values["billing_address_id"] && (addToCartData?.is_digital_only || values["shipping_address_id"])) {
       values["variation_id"] = "";
 
       const targetObject = {
         ...values,
         coupon: values["coupon"] || "",
         billing_address_id: values["billing_address_id"],
-        shipping_address_id: values["shipping_address_id"],
+        shipping_address_id: values["shipping_address_id"] || values["billing_address_id"],
         billing_address: values["billing_address"] || {},
-        shipping_address: values["shipping_address"] || {},
+        shipping_address: values["shipping_address"] || values["billing_address"] || {},
         delivery_description: values["delivery_description"] || "standard",
         delivery_interval: values["delivery_interval"] || "",
         points_amount: values["points_amount"] || 0,
@@ -55,19 +55,6 @@ const CheckoutSidebar = ({ addToCartData, values, setFieldValue, data, loading, 
         setFieldValue("coupon", "");
       }
     }
-    if (addToCartData?.is_digital_only && values["billing_address_id"] && values["payment_method"]) {
-      const targetObject = {
-        ...values,
-        coupon: values["coupon"] || "",
-        billing_address_id: values["billing_address_id"],
-        billing_address: values["billing_address"] || {},
-        points_amount: values["points_amount"] || 0,
-        payment_method: values["payment_method"] || "cod",
-        products: values["products"],
-        wallet_balance: values["wallet_balance"] || 0,
-      };
-      mutate(targetObject);
-    }
   }, [values["billing_address_id"], values["shipping_address_id"], values["payment_method"], values["delivery_description"], values["points_amount"], values["wallet_balance"]]);
   return (
     <Card className="pos-detail-card">
@@ -76,23 +63,23 @@ const CheckoutSidebar = ({ addToCartData, values, setFieldValue, data, loading, 
         <ul className={`summary-total`}>
           <li>
             <h4>{t("subtotal")}</h4>
-            <h4 className="price">{data?.data?.total?.sub_total ? convertCurrency(data?.data?.total?.sub_total) : t(`not_calculated_yet`)}</h4>
+            <h4 className="price">{typeof data?.data?.total?.sub_total !== "undefined" ? convertCurrency(data?.data?.total?.sub_total) : addToCartData?.total?.sub_total ? convertCurrency(addToCartData?.total?.sub_total) : t(`not_calculated_yet`)}</h4>
           </li>
           <li>
             <h4>{t("shipping")}</h4>
-            <h4 className="price">{data?.data?.total?.shipping_total >= 0 ? convertCurrency(data?.data?.total?.shipping_total) : t(`not_calculated_yet`)}</h4>
+            <h4 className="price">{typeof data?.data?.total?.shipping_total !== "undefined" ? convertCurrency(data?.data?.total?.shipping_total) : t(`not_calculated_yet`)}</h4>
           </li>
           <li>
             <h4>{t("tax")}</h4>
-            <h4 className="price">{data?.data?.total?.tax_total ? convertCurrency(data?.data?.total?.tax_total) : t(`not_calculated_yet`)}</h4>
+            <h4 className="price">{typeof data?.data?.total?.tax_total !== "undefined" ? convertCurrency(data?.data?.total?.tax_total) : t(`not_calculated_yet`)}</h4>
           </li>
 
           <PointWallet values={values} setFieldValue={setFieldValue} data={data} />
-          {values["consumer_id"] && values["billing_address_id"] && values["shipping_address_id"] && values["payment_method"] && values["delivery_description"] && <ApplyCoupon errorCoupon={errorCoupon} values={values} setFieldValue={setFieldValue} data={data} storeCoupon={storeCoupon} setStoreCoupon={setStoreCoupon} mutate={mutate} isLoading={loading} appliedCoupon={appliedCoupon} setAppliedCoupon={setAppliedCoupon} />}
+          {values["consumer_id"] && values["billing_address_id"] && (addToCartData?.is_digital_only || values["shipping_address_id"]) && values["payment_method"] && values["delivery_description"] && <ApplyCoupon errorCoupon={errorCoupon} values={values} setFieldValue={setFieldValue} data={data} storeCoupon={storeCoupon} setStoreCoupon={setStoreCoupon} mutate={mutate} isLoading={loading} appliedCoupon={appliedCoupon} setAppliedCoupon={setAppliedCoupon} />}
 
           <li className="list-total">
             <h4>{t("total")}</h4>
-            <h4 className="price">{data?.data?.total?.total ? convertCurrency(data?.data?.total?.total) : t(`not_calculated_yet`)}</h4>
+            <h4 className="price">{typeof data?.data?.total?.total !== "undefined" ? convertCurrency(data?.data?.total?.total) : addToCartData?.total?.total ? convertCurrency(addToCartData?.total?.total) : t(`not_calculated_yet`)}</h4>
           </li>
         </ul>
       </div>
